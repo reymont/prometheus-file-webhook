0745d9c60d21931f3ae5c014aad2009a08ac62c2
reymont <258227346@qq.com>
2018-02-02 11:08:23 +0800

    短信文件接口

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
-# A Simple HTTP Server to act as  http webhook for prometheus
+# 安装
 
-A nodejs server running on port 8080 wwhich writes body received in HTTP to local ./logs directory
+依据约定，prometheus-file-webhook收到alertmanger的告警信息，组装字段到./logs/remark.log
 
-This was used to write alerts on file for later use.
-
-## Build and run in openshift
-
-```
-oc project monitoring
-oc new-build  openshift/nodejs~https://github.com/akram/prometheus-file-webhook.git --name=file-webhook
-oc new-app file-webhook
-oc volume --add dc/file-webhook -t pvc --name=alerts --type=persistentVolumeClaim \
-          --claim-name=alerts --claim-size='1Gi' --mount-path=/opt/app-root/src/logs
+```sh
+# 安装nodejs
+yum install -y nodejs
+# 执行应用
+cd prometheus-file-webhook
+node node.js
 ```
 
-## Configure alertmanager 
+# 配置
 
-Use this config map for alertmanager 
+配置文件为config.json
 
-
+```json
+{
+    "file_path":"./logs/",//生成短信日志目录
+    "file_name":"remark.log",//生成端口文件名称
+    "port":"8188",//prometheus-file-webhook端口
+    "phone":"18600000000"//接收短信的手机号
+}
 ```
+
+默认记录从alertmanager过来的数据到 ./logs/alerts.log。短信发送的文本放在 ./logs/remark.log
+
+# 配置 alertmanager 
+
+此步骤是配置alertmanger。prometheus-file-webhook 本身是不需要做此步骤
+
+```yml
 global:
   resolve_timeout: 5m
 
@@ -32,8 +42,7 @@ route:
 receivers:
 - name: 'web.hook'
   webhook_configs:
-  - url: 'http://file-webhook.monitoring.svc.cluster.local:8080/'
-  
+  - url: 'http://promethues-webhook.local:8188/'
 ```
 
   
--- node.js
+++ node.js
@@ -1,12 +1,40 @@
 var http = require('http');
 var fs = require('fs');
-var port = 8080;
+var config = require('./config.json');
+var path = require('path');
+
+var port = config.port;
 var ip = '0.0.0.0';
 
+//使用时第二个参数可以忽略  
+function mkdir(dirpath, dirname) {
+    //判断是否是第一次调用  
+    if (typeof dirname === "undefined") {
+        if (fs.existsSync(dirpath)) {
+            return;
+        } else {
+            mkdir(dirpath, path.dirname(dirpath));
+        }
+    } else {
+        //判断第二个参数是否正常，避免调用时传入错误参数  
+        if (dirname !== path.dirname(dirpath)) {
+            mkdir(dirpath);
+            return;
+        }
+        if (fs.existsSync(dirname)) {
+            fs.mkdirSync(dirpath)
+        } else {
+            mkdir(dirname, path.dirname(dirname));
+            fs.mkdirSync(dirpath);
+        }
+    }
+}
+//mkdir('./home/ec/a/b/c/d/');
+mkdir('./logs/')
+mkdir(config.file_path)
+
 http.createServer(function (req, res) {
-
     console.log('Request Received');
-
     var body = '';
 
     res.writeHead(200, {
@@ -18,13 +46,35 @@ http.createServer(function (req, res) {
         body += chunk;
     });
 
-    req.on('end', function() {
-        fs.appendFile('logs/alerts.log', body + '\n', 'utf8');
+    req.on('end', function () {
+        var result=JSON.parse(body);
+        console.log(result.alerts)
+        var arr = result.alerts;
+        var state = arr[0].status;
+
+        var remark =  "86\t"+config.phone+"\t";
+        for(var i=0;i<arr.length; i++){  
+            // console.log("-----status------"+arr[i].status)  
+            // console.log(arr[i].labels.alertname)
+            // console.log("annotations: "+arr[i].annotations.summary)
+            // console.log("annotations: "+ db)  
+            // console.log("alertname: "+db.labels)  
+            remark +=arr[i].annotations.summary
+        }
+
+        fs.appendFile("./logs/alerts.log", body + '\n', 'utf8', function (err) {
+            if (err) {
+                console.log(err.message);
+            }
+        });
+
+        fs.appendFile(config.file_path + config.file_name, remark + '\n', 'utf8', function (err) {
+            if (err) {
+                console.log(err.message);
+            }
+        });
         res.end('{"msg": "OK"}');
     })
 
-
-
-}).listen(port, ip); console.log('Server running at http://' + ip + ':' + port);
-
-
+}).listen(port, ip);
+console.log('Server running at http://' + ip + ':' + port);
